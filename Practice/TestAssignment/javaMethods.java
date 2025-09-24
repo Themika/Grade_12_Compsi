07be6e84b7b68633acb99ee2e401e34416599708
Themika <88339133+Themika@users.noreply.github.com>
2025-09-24 10:03:22 -0400

    Add javaMethods class with various utility methods for string and number operations

---
+++ Practice/TestAssignment/javaMethods.java
@@ -0,0 +1,56 @@
+package Practice.TestAssignment;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class javaMethods {
+    public static void wordLength(ArrayList<String> words) {
+        for (String word : words) {
+            System.out.println("The length of the word \"" + word + "\" is: " + word.length());
+        }
+    }
+    public static int searchForInt(ArrayList<Integer> numbers, int target) {
+        if (numbers.contains(target)) {
+            System.out.println("Found " + target + " in the list.");
+            return 1;
+        } else {
+            System.out.println("Did not find " + target + " in the list.");
+            return -1;
+        }
+    }
+    public static void combineString(String str1, String str2) {
+        String combined = str1 + " " + str2;
+        System.out.println(combined);
+    }
+    public static void factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        System.out.println("The factorial of " + n + " is: " + result);
+    }
+    public static void removeEvens(ArrayList<Integer> numbers) {
+        numbers.removeIf(num -> num % 2 == 0);
+        System.out.println("List after removing even numbers: " + numbers);
+    }
+    public static void longestWord(ArrayList<String> words) {
+        String longest = "";
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+        System.out.println("The longest word is: " + longest);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        System.out.println("Enter the amount of words you want to find the length of: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter word " + (i + 1) + ": ");
+            String word = sc.next();
+            list.add(word);
+        }
+        sc.close();
+        wordLength(list);
+    }
+}
