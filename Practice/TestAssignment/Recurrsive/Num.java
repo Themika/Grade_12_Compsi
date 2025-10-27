880a6178c7c8d7e7048e28bf4d390b6551d7e563
Themika <88339133+Themika@users.noreply.github.com>
2025-10-27 10:32:51 -0400

    Enhance Num class: add binary conversion, string reversal, and Fibonacci calculation methods

--- Practice/TestAssignment/Recurrsive/Num.java
+++ Practice/TestAssignment/Recurrsive/Num.java
@@ -2,7 +2,10 @@ package Practice.TestAssignment.Recurrsive;
 
 public class Num {
     public static void main(String[] args) {
+        System.out.println(binary(37));
         System.out.println(sumOfNumber(5));
+        reverseString("Hello");
+        System.out.println(f(5));
     }
 
     private static int sumOfNumber(int n) {
@@ -10,5 +13,20 @@ public class Num {
         return n + sumOfNumber(n-1);
     }
 
-    
+    public static int binary(int num){
+        if(num==0) return 0;
+        return num % 2 + 10*binary(num/2);
+    }
+
+    public static void reverseString(String str){
+        if(str.length() ==0) return;
+        System.out.print(str.charAt(str.length()-1));
+        reverseString(str.substring(0,str.length()-1));
+    }
+    public static int f(int n){
+    if (n<=1) return 1;
+    System.out.println(n);
+    return f(n-1)+f(n-2);
+}
+
 }
